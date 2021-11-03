fdfe7d63a7063ce308b57e851d83a481dde13f19
Filippo Gangi Dino <filippo.gangidino@gmail.com>
2021-11-03 22:30:49 +0100

    Init with first components

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import BgCards from './components/BgCards.svelte'
+import Blob from './components/Blob.svelte'
+import Blobs from './components/Blobs.svelte'
+import BlockWithImage from './components/BlockWithImage.svelte'
+import Bubbles from './components/Bubbles.svelte'
+import Cards from './components/Cards.svelte'
+import Carousel from './components/Carousel.svelte'
+import Hero from './components/Hero.svelte'
+import Spacer from './components/Spacer.svelte'
+import SwiperNavigationButtons from './components/SwiperNavigationButtons.svelte'
+import Text from './components/Text.svelte'
+import Title from './components/Title.svelte'
+
+export { BgCards, Blob, Blobs, BlockWithImage, Bubbles, Cards, Carousel, Hero, Spacer, SwiperNavigationButtons, Text, Title }
---
+++ src/utilities.js
@@ -0,0 +1,58 @@
+export let generateBlob = (from = 25, to = 75) => {
+  let p1 = random(from, to);
+  let p2 = random(from, to);
+  let p3 = random(from, to);
+  let p4 = random(from, to);
+  return `${p1}% ${reflect(p1)}% ${reflect(p2)}% ${p2}% / ${p3}% ${p4}% ${reflect(p4)}% ${reflect(p3)}%`;
+}
+
+export let random = (from = 25, to = 75) => { return Math.floor(Math.random() * (to - from) + from) }
+
+export let reflect = (n) => { return 100 - n }
+
+export let smoothScroll = () => { 
+  let buttons = document.querySelectorAll('[data-smoothscroll]')
+  buttons.forEach((button) => {
+    button.addEventListener('click', function () {
+      open = false
+      let target = this.getAttribute('data-smoothscroll')
+      let headerOffset = 20
+      if (window.innerWidth < 1024) { 
+        headerOffset = document.querySelector('header.header').offsetHeight
+      }
+      
+      let offsetPosition = document.querySelector(target).offsetTop - headerOffset
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+      });  
+    })
+  })
+  return buttons
+}
+
+export let swiperBreakpoints = {
+  '1': {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  '480': {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  '720': {
+    slidesPerView: 2,
+    spaceBetween: 0,
+  },
+  '1024': {
+    slidesPerView: 3,
+    spaceBetween: 0,
+  },
+}
+
+function randomString() { return Math.random().toString(16).substr(2, 8) }
+
+export let swiperNavigation = (baseClass) => { 
+  let hash = randomString()
+  return { 'navigation': { nextEl: `.${baseClass}__next--${hash}`, prevEl: `.${baseClass}__prev--${hash}` }, 'hash': hash }
+}
